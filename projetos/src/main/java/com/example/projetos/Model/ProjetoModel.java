101b555f190d58b4dd97375da896d9bf27e835df
Gabriel Delefrati <168195936+GabrielDelefratiFerreira@users.noreply.github.com>
2026-03-24 11:38:46 -0400

    Model criada

---
+++ projetos/src/main/java/com/example/projetos/Model/ProjetoModel.java
@@ -0,0 +1,54 @@
+package com.example.projetos.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_projeto")
+public class ProjetoModel {
+    private Long id;
+    private String nome;
+
+    @Column(name = "Inicio_do_Projeto")
+    private LocalDate dataInicio;
+    @Column(name = "Fim_do_projeto")
+    private LocalDate dataFim;
+
+    public ProjetoModel(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+}
